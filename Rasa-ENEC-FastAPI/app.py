c364059b86f8a7cc9d900945930d640e24f888d5
Diwakar <diwakarnehrudasan@gmail.com>
2023-10-30 12:00:35 +0530

    dummy push

--- Rasa-ENEC-FastAPI/app.py
+++ Rasa-ENEC-FastAPI/app.py
@@ -195,6 +195,15 @@ class ENEC_rejected_invoice_item_info(BaseModel):
     inv_no : str
     username : str
 
+
+class ENEC_Pending_SES_List(BaseModel):
+    username : str
+
+class ENEC_SES_DETAILS(BaseModel):
+    username: str
+    ses_no : str
+
+
 # ************************************* Dashbodard Class ******************************************************************
 
 class ENEC_Total_PR_req_count(BaseModel):
@@ -1841,6 +1850,69 @@ def ENEC_rejected_invoice_item_info(data:ENEC_rejected_invoice_item_info):
 
 
     return invoice_info
+
+
+@app.post('/ENEC_Pending_SES_List')
+def ENEC_Pending_SES_List(data:ENEC_Pending_SES_List):
+
+    url = 'http://dxbktlds4.kaarcloud.com:8000/sap/bc/srt/wsdl/flv_10002A111AD1/srvc_url/sap/bc/srt/scs/sap/zbapi_ses_pending?sap-client=100'
+
+    transport = HttpAuthenticated(username=sap_username, password=sap_password)
+    client = Client(url,transport=transport)
+    result = client.service.ZMM_SES_PENDING_FM(data.username)
+
+    listofobj = result[0]
+    # print(type(listofobj))
+
+
+    pending_ses_list = []
+
+
+    for i in listofobj:
+        pending_ses_dict = {}
+        pending_ses_dict['ENTRYSHEET_NO'] = "SES " + str(i['ENTRYSHEET'])
+        pending_ses_dict['CREATED_ON'] = i['CREATED_ON']
+        pending_ses_dict['CREATED_BY'] = i['CREATED_BY']
+        # print(pending_ses_dict)
+        pending_ses_list.append(pending_ses_dict)
+
+
+    return pending_ses_list
+
+@app.post('/ENEC_SES_DETAILS')
+async def ENEC_SES_DETAILS(data:ENEC_SES_DETAILS):
+
+    url = 'http://dxbktlds4.kaarcloud.com:8000/sap/bc/srt/wsdl/flv_10002A111AD1/srvc_url/sap/bc/srt/scs/sap/zbapi_ses_get_detail_time?sap-client=100'
+
+    transport = HttpAuthenticated(username=sap_username, password=sap_password)
+    client = Client(url,transport=transport)
+    result = client.service.ZMM_SES_GET_DETAIL_FM(data.ses_no) 
+    # print(result)
+    # print(result[0])
+
+    data = result[0]
+
+    SES_DETAILS = {}
+
+    SES_DETAILS["SHEET_NO"] =  data["SHEET_NO"]
+    SES_DETAILS["CREATED_BY"] = data["CREATED_BY"]
+    SES_DETAILS["CREATED_ON"] = data["CREATED_ON"]
+    SES_DETAILS["CURRENCY"] = data["CURRENCY"]
+    SES_DETAILS["PO_NUMBER"] = data["PO_NUMBER"]
+    SES_DETAILS["PO_ITEM"] = data["PO_ITEM"]
+    SES_DETAILS["SHORT_TEXT"] = data["SHORT_TEXT"]
+    SES_DETAILS["PCKG_NO"] = data["PCKG_NO"]
+    SES_DETAILS["NET_VALUE"] = data["NET_VALUE"]
+
+    return SES_DETAILS
+
+
+
+
+
+
+
+
 
 
 # ********************************************************** Dashboard API **********************************************************
